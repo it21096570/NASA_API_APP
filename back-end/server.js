2fc46f84112aa26b13492b69e63617391e68c880
it21096570 <it21096570@my.sliit.lk>
2024-05-03 07:56:59 +0530

    deploy

--- back-end/server.js
+++ back-end/server.js
@@ -39,13 +39,6 @@ pool.getConnection((err, connection) => {
     }
 });
 
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "front-end", "build"))); app.get("*", (req, res) => {
-    });
-    res.sendFile(path.join(__dirname, "front-end", "build", "index.html"));
-}
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
